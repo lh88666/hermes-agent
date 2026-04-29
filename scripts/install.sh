c214f9a681b1952b1ef68859ae5c37077f1e7e8b
CC Leon <127478084+lh88666@users.noreply.github.com>
2026-04-29 16:25:37 +0800

    更改ssh地址

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,7 +26,7 @@ NC='\033[0m' # No Color
 BOLD='\033[1m'
 
 # Configuration
-REPO_URL_SSH="git@github.com:NousResearch/hermes-agent.git"
+REPO_URL_SSH="git@github.com:lh88666/hermes-agent.git"
 REPO_URL_HTTPS="https://github.com/NousResearch/hermes-agent.git"
 HERMES_HOME="${HERMES_HOME:-$HOME/.hermes}"
 # INSTALL_DIR is resolved AFTER arg parsing and OS detection so we can pick an
